f6973cb34772ea2fcbb78a637212bf2f62b435ac
christal <christal.b.camillo@gmail.com>
2021-08-17 18:45:49 -0300

    norminette

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: christalbruneli <christalbruneli@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 19:32:40 by christalbru       #+#    #+#             */
-/*   Updated: 2021/08/16 20:02:02 by christalbru      ###   ########.fr       */
+/*   Updated: 2021/08/17 18:45:16 by christalbru      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-void    ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-    ft_memset(s, 0, n);
+	ft_memset(s, 0, n);
 }
