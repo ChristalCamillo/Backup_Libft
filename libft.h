5bc4ff7a3503556b3300bcb9cbf28522e5ca4fce
christal <christal.b.camillo@gmail.com>
2021-08-18 17:34:48 -0300

    final

--- libft.h
+++ libft.h
@@ -75,21 +75,20 @@ void				*ft_memset(void *buffer, int c, size_t len);
 ** --- NUMERIC CONVERSION FUNCTIONS ---
 */
 char				*ft_itoa(int n);
-char				*ft_itoa_base(int n, int base);
-int					ft_atoi_base(const char *str, int base);
-int					ft_numdig(int n, int base);
 /*
 ** --- STRING MANIPULATION FUNCTIONS ---
 */
 char				*ft_strjoin(char const *s1, char const *s2);
-char				*ft_strmap(char const *s, char (*f)(char));
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char				*ft_strnew(size_t size);
 char				*ft_strtrim(char const *s1, char const *set);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				**ft_split(char const *s, char c);
-void				ft_strdel(char **str);
-void				ft_strclr(char *str);
-void				ft_striter(char *s, void (*f)(char *));
 void				ft_striteri(char *s, void (*f)(unsigned int, char *));
+/*
+** --- FILE DESCRIPTOR FUNCTIONS ---
+*/
+void				ft_putchar_fd(char c, int fd);
+void				ft_putendl_fd(char *s, int fd);
+void				ft_putnbr_fd(int n, int fd);
+void				ft_putstr_fd(char *s, int fd);
 #endif
